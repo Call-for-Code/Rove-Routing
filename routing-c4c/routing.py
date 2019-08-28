dc89f83bb0bc8f5029665242d7c23194ca42547d
Lucas Xia <lucasxia01@gmail.com>
2019-08-27 17:23:04 -0700

    bugs are fixed, routing works

--- routing-c4c/routing.py
+++ routing-c4c/routing.py
@@ -3,16 +3,30 @@ import sys
 from flask import jsonify
 import json
 import os
+import time
 
 def checkIntersectionSeg(seg1, seg2):
 	slope1 = (seg1['lng2']-seg1['lng1'])*(seg2['lat2']-seg2['lat1'])
 	slope2 = (seg2['lng2']-seg2['lng1'])*(seg1['lat2']-seg1['lat1'])
+	if slope1 == slope2:
+		return False
+
 	interm = slope2*seg2['lat1']-slope1*seg1['lat1']+(seg2['lat2']-seg2['lat1'])*(seg1['lat2']-seg1['lat1'])*(seg1['lng1']-seg2['lng1'])
 	latInt = interm/(slope2-slope1)
 	# print(latInt)
-	seg1Diff = (latInt-seg1['lat1'])*(latInt-seg1['lat2'])
-	seg2Diff = (latInt-seg2['lat1'])*(latInt-seg2['lat2'])
-	return (seg1Diff <= 0 and seg2Diff <= 0)
+
+	seg1XDiff = (latInt-seg1['lat1'])*(latInt-seg1['lat2'])
+	seg2XDiff = (latInt-seg2['lat1'])*(latInt-seg2['lat2'])
+
+	if (seg1['lat2']-seg1['lat1']) != 0 and (seg2['lat2']-seg2['lat1']) != 0:
+		return (seg1XDiff <= 0 and seg2XDiff <= 0)
+	elif (seg2['lat2']-seg2['lat1']) == 0:
+		lngInt = (seg1['lng2']-seg1['lng1'])/(seg1['lat2']-seg1['lat1']) * (latInt - seg1['lat1']) + seg1['lng1']
+	else:
+		lngInt = (seg2['lng2']-seg2['lng1'])/(seg2['lat2']-seg2['lat1']) * (latInt - seg2['lat1']) + seg2['lng1']
+	seg1YDiff = (lngInt-seg1['lng1'])*(lngInt-seg1['lng2'])
+	seg2YDiff = (lngInt-seg2['lng1'])*(lngInt-seg2['lng2'])
+	return (seg1XDiff <= 0 and seg2XDiff <= 0) and (seg1YDiff <= 0 and seg2YDiff <= 0)
 
 def checkIntersectionRect(seg1, seg2):
 	return
@@ -50,10 +64,10 @@ def getBlockedRoads():
 					if i != 0 and i != len(coords)-1:
 						roads.append(coords[i][1])
 						roads.append(coords[i][0])
-	roads.append(29.82);
-	roads.append(-95.46231);
-	roads.append(29.85);
-	roads.append(-95.45988);
+	# roads.append(29.82);
+	# roads.append(-95.46231);
+	# roads.append(29.85);
+	# roads.append(-95.45988);
 	return roads
 
 def processBlockedRoads():
@@ -105,6 +119,8 @@ def routePath(start, end):
 	APP_ID = os.getenv("APP_ID")
 	APP_CODE = os.getenv("APP_CODE")
 	for i in range(21): # max 20 rectangles allowed by the API
+		print(strRects)
+		start_time = time.time()
 		res = requests.get('https://route.api.here.com/routing/7.2/calculateroute.json', 
 			params = {'app_id':APP_ID,
 						'app_code':APP_CODE,
@@ -115,6 +131,8 @@ def routePath(start, end):
 						'avoidareas':strRects
 					}
 				)
+		end_time = time.time()
+		print("API CALL TIME:", end_time-start_time)
 		if not res:
 			print(res.json())
 			sys.exit("Error with request")
@@ -122,6 +140,7 @@ def routePath(start, end):
 		route = res.json()['response']['route'][0]['shape']
 		routeSegs = []
 		anyBlockage = False
+		start_time = time.time()
 		for j in range(len(route)-1):
 			point1 = route[j].split(',')
 			point2 = route[j+1].split(',')
@@ -141,38 +160,42 @@ def routePath(start, end):
 			if anyBlockage: # blocking happened
 				break
 		
-		
+		end_time = time.time()
+		print("PROCESSING TIME:", end_time-start_time)
 		if not anyBlockage:
 			break
 		print("Path still blocked " + str(i))
 		if strRects != "":
 			strRects += "!"
-		roadBlock = calculateRectangles(roadBlock)
+		print(len(route))
 		print(roadBlock)
+		roadBlock = calculateRectangles(roadBlock)
 		strRects += str(roadBlock['lat1']) + ',' + str(roadBlock['lng1']) + ";" + str(roadBlock['lat2']) + ',' + str(roadBlock['lng2'])
 	if anyBlockage:
 		print(strRects)
 		print("Cannot avoid blocked roads")
+	else:
+		print("Avoids all blocked roads!")
 	i = 0
-	for coords in route:
-		i += 1
-		#print(coords + "," + str(i) + ",#00FF00")
+	# for coords in route:
+	# 	i += 1
+	#	print(coords + "," + str(i) + ",#00FF00")
 
-	seg1 = {
-		'lat1': 29.832296,
-		'lng1': -95.451352,
-		'lat2': 29.841382,
-		'lng2': -95.488790
-	}
-	seg2 = {
-		'lat1': 29.82,
-		'lng1': -95.46231,
-		'lat2': 29.85,
-		'lng2': -95.45988
-	}
-	print(checkIntersectionSeg(seg1, seg2))
+	# seg1 = {
+	# 	'lat1': 29.832296,
+	# 	'lng1': -95.451352,
+	# 	'lat2': 29.841382,
+	# 	'lng2': -95.488790
+	# }
+	# seg2 = {
+	# 	'lat1': 29.82,
+	# 	'lng1': -95.46231,
+	# 	'lat2': 29.85,
+	# 	'lng2': -95.45988
+	# }
+	# print(checkIntersectionSeg(seg1, seg2))
 	
-
+	print(jsonify({'route': route}))
 	return jsonify({'route': route})
 
 if __name__ == '__main__':
